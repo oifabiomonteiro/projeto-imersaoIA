5cc0455c5918ee771d915c8b19acaf03f474ab52
oifabiomonteiro <111027981+oifabiomonteiro@users.noreply.github.com>
2024-09-06 23:22:50 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 function pesquisar() {
     // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value
+
+    //se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>nada foi encontrado Digite o nome do personagem</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
     
-    let resultados = ""; // Inicializa uma string vazia para armazenar os resultados
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = "";
+    let descricao = ""; 
 
     // Itera sobre cada dado na lista de dados
     for (let dado of dados) {
-        // Cria uma nova div para cada resultado
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        //se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+            // Cria uma novo elemento 
+            resultados += `
             <div class="item-resultado">
-                <h2>${dado.titulo}</h2> <p class="descricao-meta">${dado.descricao}</p> <a href=${dado.link} target="_blank">Saiba mais</a> </div>
+                <h2>${dado.titulo}</h2> <p class="descricao-meta">${dado.descricao}</p> <a href=${dado.link} target="_blank">Saiba mais</a> 
+            </div>
         `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
     }
 
     // Atribui os resultados gerados ao conteúdo da seção
--- dados.js
+++ dados.js
@@ -7,13 +7,13 @@ let dados = [
     },
     {
         titulo : "Daemon Targaryen", 
-        descricao : "Daemon Targaryen era um príncipe complexo e controverso, conhecido por sua natureza rebelde e ambiciosa. Tio de Rhaenyra, ele era um guerreiro talentoso e um cavaleiro valente, mas também demonstrava um lado cruel e imprevisível. Sua lealdade a Rhaenyra era inquestionável, mas seus métodos frequentemente colocavam em risco seus objetivos.",
+        descricao : "Daemon Targaryen era um príncipe complexo e controverso, conhecido por sua natureza rebelde e ambiciosa. Ele era um guerreiro talentoso e um cavaleiro valente, mas também demonstrava um lado cruel e imprevisível. Sua lealdade era inquestionável, mas seus métodos frequentemente colocavam em risco seus objetivos.",
         link : "https://wiki.geloefogo.com/index.php/Daemon_Targaryen",
     
     },
     {
         titulo : "Alicent Hightower", 
-        descricao : "Alicent Hightower foi uma figura central na Casa do Dragão, servindo como a segunda esposa do Rei Viserys I Targaryen. Inicialmente uma amiga de infância de Rhaenyra, Alicent viu sua lealdade ao rei e à Coroa ser testada quando Rhaenyra foi nomeada herdeira. Com a ascensão de Rhaenyra, Alicent e seus filhos se sentiram traídos e marginalizados, o que desencadeou uma profunda rivalidade entre as duas mulheres.",
+        descricao : "Alicent Hightower foi uma figura central na Casa do Dragão, servindo como a segunda esposa do Rei Viserys I Targaryen. Inicialmente uma amiga de infância da filha de Viserys, Alicent viu sua lealdade ao rei e à Coroa ser testada quando a filha de Vicerys I foi nomeada herdeira. Com a ascensão da filha, Alicent e seus filhos se sentiram traídos e marginalizados, o que desencadeou uma profunda rivalidade entre as duas mulheres.",
         link : "https://wiki.geloefogo.com/index.php/Alicent_Hightower",
     
     },   
